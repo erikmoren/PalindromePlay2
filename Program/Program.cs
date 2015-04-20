49e1d9ff26d2d0e74a70f3131accad2471dc76e8
unclebobsweden <erik.moren@gmail.com>
2015-04-20 09:43:51 +0200

    Initial commit

---
+++ Program/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PalindromeFinder p = new PalindromeFinder();
+            while (true)
+            {
+
+                Console.Write("Enter a nice word: ");
+                var value = Console.ReadLine();
+                if (value == "q")
+                    break;
+                try
+                {
+                    if (p.IsPalindrome(value))
+                    {
+                        Console.WriteLine("Yey! The word is a palindrome.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The word is not a plaindrome.\n");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ TDDPalindrome.Test/PalindromeTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDPalindrome.Test
+{
+    [TestClass]
+    public class PalindromeTests
+    {
+        private PalindromeFinder _palindromeFinder = null;
+        
+        [TestInitialize]
+        public void InitTest()
+        {
+            _palindromeFinder = new PalindromeFinder();
+        }
+
+        #region Positive Cases
+        [TestMethod]
+        public void IsPalindromeReturnTrue()
+        {
+            //Arrange
+            string value = "rotator";
+            bool expected = true;
+
+            //Act
+            bool actual = _palindromeFinder.IsPalindrome(value);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+        #endregion Positive Cases
+
+        #region Negative Cases
+        [TestMethod]
+        public void IsPalindromeReturnFalse()
+        {
+            //Arrange
+            string value = "raggartangenter";
+            bool expected = false;
+
+            //Act
+            bool actual = _palindromeFinder.IsPalindrome(value);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IsPalindromeThrowsArgumentNullException()
+        {
+            //Arrange
+            string value = null;
+
+            //Act
+            bool actual = _palindromeFinder.IsPalindrome(value);
+
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void IsPalindromeThrowsArgumentException()
+        {
+            //Arrange
+            string value = "a";
+
+            //Act
+            bool actual = _palindromeFinder.IsPalindrome(value);
+
+        }
+
+
+
+        #endregion Negative Cases
+
+
+
+        #region Impossible Cases
+        [TestMethod]
+        public void ImpossibleMission()
+        {
+            //Assert
+            Assert.AreEqual(0, 1);
+        }
+        #endregion Impossible Cases
+
+
+    }
+}
